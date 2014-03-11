6fcb4fe98ae9afb7632f7b87f8374ce0084398b7
Mark Muetzelfeldt <markmuetz@gmail.com>
2014-03-11 22:55:04 +0000

    Make problem1 a little more general.
    It now solves problem one for the general case of n elements.
    Unfortunately, the solutions don't converge, in fact, as you
    increase n, the value at T1 goes higher than T5. I'm pretty sure
    this is unphysical and wrong!

--- problem1/project.py
+++ problem1/project.py
@@ -7,12 +7,66 @@ T_0   = 250
 T_inf = 75
 L     = 4
 
-def generate_M():
-    M = np.matrix([[ 1/2 + ALPHA/3, -1/2 + ALPHA/6,              0,              0],
-                  [-1/2 + ALPHA/6,  2 + 2*ALPHA/3, -3/2 + ALPHA/6,              0],
-                  [             0, -3/2 + ALPHA/6,  4 + 2*ALPHA/3, -5/2 + ALPHA/6],
-                  [             0,              0, -5/2 + ALPHA/6,  6 + 2*ALPHA/3]])
-    return M
+def generate_K(n):
+    if n < 2:
+        raise "n must be >= 2"
+    K = np.matrix(np.zeros((n + 1, n + 1)))
+    K[0, 0] = 1
+    K[0, 1] = -1
+    K[1, 0] = K[0, 1]
+    for i in range(n - 1):
+        K[1 + i, 1 + i] = (i + 1) * 4
+        K[1 + i, 2 + i] = -3 - 2 * i
+        K[2 + i, 1 + i] = K[1 + i, 2 + i] 
+    K[n - 1, n] = -((n + 1) * 2 - 3)
+    K[n, n - 1] = K[n - 1, n]
+    K[n, n] = -K[n - 1, n]
+    K *= L**2 / (2 * n**2)
+    #validate_K(K)
+    return K
+
+def generate_fl(n):
+    fl1 = np.matrix(np.zeros((n + 1, 1)))
+    fl1[0, 0] = 1
+    fl1[-1, 0] = 1
+    fl1[1:-1, 0] = 2
+    fl1 *= ALPHA * T_inf * L**2 / (2 * n**2)
+
+    fl2 = np.matrix(np.zeros((n + 1, n + 1)))
+    fl2[0, 0] = 2
+    fl2[0, 1] = 1
+    fl2[1, 0] = fl2[0, 1]
+    fl2[-1, -1] = 2
+    for i in range(n - 1):
+        fl2[1 + i, 2 + i] = 1
+        fl2[2 + i, 1 + i] = 1
+        fl2[1 + i, 1 + i] = 4
+
+    fl2 *= ALPHA * L**3 / (6 * n**3)
+
+    return fl1, fl2
+
+def validate_K(K):
+    if K.shape[0] != K.shape[1]:
+        raise Exception("K is not square")
+    # This test causes problems for some reason.
+    #if np.linalg.det(K) != 0:
+        #raise Exception("det(K) != 0")
+    for i in range(len(K)):
+        if K[i].sum() != 0:
+            raise Exception("row %i != 0"%i)
+        if K[:, i].sum() != 0:
+            raise Exception("col %i != 0"%i)
+        for j in range(i):
+            if K[i, j] != K[j, i]:
+                raise Exception("matrix not symmetrix")
+
+def generate_R():
+    R = np.matrix([[ 1/2 + ALPHA/3, -1/2 + ALPHA/6,              0,              0],
+                   [-1/2 + ALPHA/6,  2 + 2*ALPHA/3, -3/2 + ALPHA/6,              0],
+                   [             0, -3/2 + ALPHA/6,  4 + 2*ALPHA/3, -5/2 + ALPHA/6],
+                   [             0,              0, -5/2 + ALPHA/6,  6 + 2*ALPHA/3]])
+    return R
 
 def generate_f_lb():
     f_lb = np.matrix([[ALPHA * T_inf / 2],
@@ -23,23 +77,47 @@ def generate_f_lb():
 
 
 
-def run_project():
-    M = generate_M()
-    f_lb = generate_f_lb()
-    T = M.I * f_lb
-    #! T_1 == T[0] to translate between code and question.
-    temperatures = [T[(i, 0)] for i in range(len(T))]
-    temperatures.append(250)
-    xs = [0, 1, 2, 3, 4]
-    plt.plot(xs, temperatures)
+def multi_run(max_n):
+    for i in range(2, max_n):
+        run_project(i, False)
     plt.show()
 
+def draw_graph(T, n, plot):
+    temperatures = [T[(i, 0)] for i in range(len(T))]
+    temperatures.append(250)
+    xs = np.arange(len(temperatures)) * 1.
+    xs *= 4. / n
+    plt.plot(xs, temperatures)
+    plt.plot(xs, temperatures, 'k+')
+    if plot:
+        plt.show()
 
+def run_project(n=4, plot=True):
+    Rs = []
+    if False:
+        # Old way
+        R = generate_R()
+        f_lb = generate_f_lb()
+        T = R.I * f_lb
+        #! T_1 == T[0] to translate between code and question.
+        draw_graph(T, 4, plot)
+        Rs.append(R)
+    
+    if True:
+        # New way
+        K = generate_K(n)
+        fl1, fl2 = generate_fl(n)
 
+        M = K + fl2
 
+        f_lb = fl1[:-1] - M[:-1, -1] * T_0
+        R = M[:-1, :-1]
 
+        T = R.I * f_lb
+        draw_graph(T, n, plot)
+        Rs.append(M)
 
-    return T
+    return Rs
 
 if __name__ == "__main__":
     run_project()
--- problem2/project.py
+++ problem2/project.py
@@ -1,7 +1,7 @@
 from __future__ import division
 
 import numpy as np
-#import pylab as plt
+import pylab as plt
 
 import distmesh as dm
 
@@ -13,11 +13,17 @@ def rectangle_with_circle_at_corner():
 def rectangle_with_circle_at_centre():
     # Does not terminate!
     fd = lambda p: dm.ddiff(dm.drectangle(p,0,20,0,12), dm.dcircle(p,10,6,4))
-    return dm.distmesh2d(fd, dm.huniform, 0.5, (0, 0, 20, 12))
+    return dm.distmesh2d(fd, dm.huniform, 0.5, (0, 0, 20, 12), max_iter=100)
+
+def simple_rect():
+    fd = lambda p: dm.drectangle(p,0,1,0,1)
+    return dm.distmesh2d(fd, dm.huniform, 0.1, (0, 0, 1, 1), max_iter=100)
 
 def run_project():
+    plt.ion()
     #return rectangle_with_circle_at_corner()
-    return rectangle_with_circle_at_centre()
+    return simple_rect()
+    #return rectangle_with_circle_at_centre()
 
 if __name__ == "__main__":
     run_project()
